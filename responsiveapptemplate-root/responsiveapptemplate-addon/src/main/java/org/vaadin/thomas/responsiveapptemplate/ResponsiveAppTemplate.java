1a578297de4c87f06167bc6ee8c77add1abee24b
thomas <thomas@vaadin.com>
2016-09-16 16:26:26 +0300

    removes old files, adds new root file

---
+++ responsiveapptemplate-root/responsiveapptemplate-addon/src/main/java/org/vaadin/thomas/responsiveapptemplate/ResponsiveAppTemplate.java
@@ -0,0 +1,12 @@
+package org.vaadin.thomas.responsiveapptemplate;
+
+import com.vaadin.ui.HorizontalLayout;
+
+public class ResponsiveAppTemplate extends HorizontalLayout {
+
+	private static final long serialVersionUID = -2732841564677007541L;
+
+	public ResponsiveAppTemplate() {
+
+	}
+}
--- responsiveapptemplate-root/responsiveapptemplate-demo/src/main/java/org/vaadin/thomas/responsiveapptemplate/demo/DemoUI.java
+++ responsiveapptemplate-root/responsiveapptemplate-demo/src/main/java/org/vaadin/thomas/responsiveapptemplate/demo/DemoUI.java
@@ -1,41 +1,32 @@
 package org.vaadin.thomas.responsiveapptemplate.demo;
 
-import org.vaadin.thomas.responsiveapptemplate.MyComponent;
-
 import javax.servlet.annotation.WebServlet;
+
+import org.vaadin.thomas.responsiveapptemplate.ResponsiveAppTemplate;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Alignment;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
 
 @Theme("demo")
 @Title("MyComponent Add-on Demo")
 @SuppressWarnings("serial")
-public class DemoUI extends UI
-{
+public class DemoUI extends UI {
 
-    @WebServlet(value = "/*", asyncSupported = true)
-    @VaadinServletConfiguration(productionMode = false, ui = DemoUI.class)
-    public static class Servlet extends VaadinServlet {
-    }
+	@WebServlet(value = "/*", asyncSupported = true)
+	@VaadinServletConfiguration(productionMode = false, ui = DemoUI.class)
+	public static class Servlet extends VaadinServlet {
+	}
 
-    @Override
-    protected void init(VaadinRequest request) {
+	@Override
+	protected void init(VaadinRequest request) {
 
-        // Initialize our new UI component
-        final MyComponent component = new MyComponent();
+		// Initialize our new UI component
+		final ResponsiveAppTemplate component = new ResponsiveAppTemplate();
 
-        // Show it in the middle of the screen
-        final VerticalLayout layout = new VerticalLayout();
-        layout.setStyleName("demoContentLayout");
-        layout.setSizeFull();
-        layout.addComponent(component);
-        layout.setComponentAlignment(component, Alignment.MIDDLE_CENTER);
-        setContent(layout);
-    }
+		setContent(component);
+	}
 }
